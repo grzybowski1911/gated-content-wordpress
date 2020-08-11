95cf2388030f779b3454d7555d357690637541b8
Admin <admin@Admins-MacBook-Pro-3.local>
2020-08-11 06:50:26 -0700

    start dev

---
+++ start.php
@@ -0,0 +1,15 @@
+<?php 
+
+/* 
+ * Plugin Name:       Gated Content
+ * Plugin URI:        bgrzdesigns.com/gated-content
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Ben Grzybowski
+ * Author URI:        bgrzdesigns.com/about
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       gated-content
+ * Domain Path:       /languages
+*/
+
